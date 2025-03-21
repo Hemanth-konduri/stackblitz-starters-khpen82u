6b7763f6f32ed7367036163ce4a59d79298897e2
Hemanth-konduri <konduri.hemanth.s89@gmail.com>
2025-03-21 12:06:44 +0530

    Connected database to backend successfully

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
 
 const app = express();
 const port = 3010;
@@ -13,3 +15,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.listen(async()=>{
+  try{
+    await mongoose.connect(process.env.MongoDB_URI);
+    console.log("Connected to database");
+  }catch(error){
+    console.log("Error connecting to database: ", error);
+  }
+})
